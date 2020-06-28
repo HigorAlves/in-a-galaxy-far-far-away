55f06ab35c589c09fa92cf1bf5e56fda92efffe8
Higor Alves <higorhaalves@gmail.com>
2020-06-28 11:39:04 -0300

    Updates

---
+++ precache-manifest.7275b245038fc7c1a5d3f3f359bae865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529c2d804cf7937577eb1d25595a61a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef02fa8867b4f5806ad",
+    "url": "/static/js/2.b0f2ef31.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/static/js/2.b0f2ef31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd7b6ac6d3cd6a32db8",
+    "url": "/static/js/main.63c46667.chunk.js"
+  },
+  {
+    "revision": "a394adc7644238d5d16e",
+    "url": "/static/js/runtime-main.e2bc2165.js"
+  },
+  {
+    "revision": "06f9b6b522d9ee29fcfdcb0cb358afbb",
+    "url": "/static/media/example.06f9b6b5.jpeg"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/static/media/stars.6494ab5b.png"
+  }
+]);
